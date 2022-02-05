c35f7578e73856eef42123c98c7904a9261f1ac7
H Krishnan <h.krishnan@ansys.com>
2022-02-05 14:07:48 +0530

    express server post

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
 const mockUserData=[
   {name: 'Mark'},
   {name: 'Jill'}
@@ -19,6 +22,25 @@ app.get('/users/:id', function(req, res) {
           message: 'got one user',
           user: req.params.id
   })
+})
+app.post('/login', function(req, res) {
+  const username=req.body.username;
+  const password=req.body.password;
+
+  const mockUsername="krish";
+  const mockPassword="nan";
+
+  if (username == mockUserName && password == mockPassword) {
+    res.json({
+             success: true,
+             message: 'password and username match!',
+             token: 'encrypted token' })
+  } else {
+      res.json({
+               success: false,
+               message: 'wrong username or password!',
+      })
+  }
 })
 
 app.listen(8000, function(){
